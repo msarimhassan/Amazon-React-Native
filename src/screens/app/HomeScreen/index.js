e541f50ad12fabeb8d955fb91cf8c78e2afca204
sarim-dev <sarimhassan5@gmail.com>
2022-08-01 17:56:53 +0500

    Add One Signal

--- src/screens/app/HomeScreen/index.js
+++ src/screens/app/HomeScreen/index.js
@@ -1,12 +1,47 @@
 import React, {useEffect, useState} from 'react';
-import {View, StyleSheet, ScrollView, FlatList} from 'react-native';
+import {View, StyleSheet, FlatList} from 'react-native';
 import {Colors} from '../../../common';
 import CategoryCard from '../../../components/Cards/CategoryCard.js';
 import {config, Network, Urls} from '../../../config';
 import AmazonLoader from '../../../../assets/animations';
 import GeoLocation from '@react-native-community/geolocation';
+import OneSignal from 'react-native-onesignal';
+import {useSelector} from 'react-redux'
+
+
 
 const HomeScreen = () => {
+  const currentUser = useSelector(state => state.auth.user);
+
+    
+  useEffect(() => {
+    // One Signal Configuration
+    OneSignal.setLogLevel(6, 0);
+    OneSignal.setAppId('ea38e390-cd6e-43a9-8d5b-1cfc79a91491');
+    OneSignal.promptForPushNotificationsWithUserResponse(response => {
+      console.log('Prompt response:', response);
+    });
+    OneSignal.setNotificationWillShowInForegroundHandler(
+      notificationReceivedEvent => {
+        console.log(
+          'OneSignal: notification will show in foreground:',
+          notificationReceivedEvent,
+        );
+        let notification = notificationReceivedEvent.getNotification();
+        console.log('notification: ', notification);
+        const data = notification.additionalData;
+        console.log('additionalData: ', data);
+        // Complete with null means don't show a notification.
+        notificationReceivedEvent.complete(notification);
+      },
+    );
+    OneSignal.setNotificationOpenedHandler(notification => {
+      console.log('OneSignal: notification opened:', notification);
+    });
+    OneSignal.setExternalUserId(currentUser?._id);
+  })
+
+
   const [categoryList, setcategoryList] = useState([]);
   const [loading, setLoading] = useState(true);
   useEffect(() => {
